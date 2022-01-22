00f31aa7adcf5740734b27cccee2b46847fdd9df
kstulgys <karolis.stulgys@gmail.com>
2022-01-22 16:42:24 +0000

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head"
 import { ChakraProvider } from "@chakra-ui/react"
+import Script from "next/script"
+
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
@@ -15,6 +17,16 @@ function MyApp({ Component, pageProps }) {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;700;900&display=swap" rel="stylesheet" />
         <script src="//code.tidio.co/ogzuurqarhap0hu77ypyxfrvanh8i03p.js" async></script>
       </Head>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} />
+      <Script strategy="lazyOnload" id="ga-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+      </Script>
+      <Script strategy="lazyOnload" src="//code.tidio.co/horcjpmh4cyjywzqwtchuceklbfnex9j.js" />
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
